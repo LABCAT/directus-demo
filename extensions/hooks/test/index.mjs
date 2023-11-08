d9302340ae5eb5436b85809cef593d73576b35d7
LABCAT <shane.watters.nz@gmail.com>
2023-11-08 22:09:25 +1300

    Updating extension to mjs.

--- extensions/hooks/test/index.mjs
+++ extensions/hooks/test/index.mjs
@@ -1,4 +1,4 @@
-module.exports = function registerHook({ action }, { services, env, exceptions }) {
+export default function registerHook({ action }, { services, env, exceptions }) {
     action('items.create', async ({ key, collection, payload }, { database, schema, accountability }) => {
         console.log(collection);
     });
