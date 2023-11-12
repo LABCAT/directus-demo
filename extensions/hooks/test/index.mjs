81c60bfbcb4ff422d13bb469b1f0ba412d12a587
LABCAT <shane.watters.nz@gmail.com>
2023-11-12 21:30:54 +1300

    Update index.mjs

--- extensions/hooks/test/index.mjs
+++ extensions/hooks/test/index.mjs
@@ -1,5 +1,6 @@
 export default function registerHook({ action }, { services, env, exceptions }) {
     action('items.create', async ({ key, collection, payload }, { database, schema, accountability }) => {
         console.log(collection);
+        console.log(payload);
     });
 };
